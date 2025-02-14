70b11c29ea10fb2c5ed6d7803dfa9b1b82ea2395
Aidan Brubeck <50123545+Ayydeeen@users.noreply.github.com>
2025-02-14 08:46:37 -0500

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -2,4 +2,7 @@ foreach($i in (get-content C:\Temp\unlinkOUtest.csv)){
   echo $i
 	Restore-MsolUser -UserPrincipalName $i
 	Set-MSOLuser -UserPrincipalName $i -ImmutableID "$null" 
+
+ 	#Uncomment below line to disable account after restored	
+  	Set-MsolUser -UserPrincipalName $i -BlockCredential $true 
 }
